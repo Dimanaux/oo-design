711d428901e5e3b7b14ddde1ddd7fcc2c153b030
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-07-12 16:16:43 +0300

    new type: Bool - completed now

--- src/main/java/oo/lang/bool/Bool.java
+++ src/main/java/oo/lang/bool/Bool.java
@@ -1,7 +1,27 @@
 package oo.lang.bool;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Bool {
+    public static final Bool False = FalseClass.False;
+    public static final Bool True = TrueClass.True;
+
+    private static final Map<Boolean, Bool> primitiveToObject;
+
+    static {
+        primitiveToObject = new HashMap<>(2);
+        primitiveToObject.put(false, False);
+        primitiveToObject.put(true, True);
+    }
+
+    public static Bool valueOf(boolean primitive) {
+        return primitiveToObject.get(primitive);
+    }
+
     private Bool() {
     }
+
+    public abstract void control(If statement);
 }
 
