e9b72a25070641d6bf10c3bf884d48ea05daeaad
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-07-12 14:03:13 +0300

    new type: If (but as expression)

--- src/main/java/oo/lang/statement/If.java
+++ src/main/java/oo/lang/statement/If.java
@@ -1,34 +1,36 @@
 package oo.lang.statement;
 
-public class If {
+import java.util.function.Supplier;
+import java.util.Optional;
+
+public class If<T> {
     private final boolean condition;
-    private boolean done = false;
+    private Optional<T> result = Optional.<T>empty();
 
     public If(boolean condition) {
         this.condition = condition;
     }
 
-    private If(boolean condition, boolean done) {
+    private If(boolean condition, Optional<T> result) {
         this(condition);
-        this.done = done;
+        this.result = result;
     }
 
-    public If then(Runnable then) {
-        if (condition && !done) {
-            then.run();
-            done = true;
+    public If then(Supplier<T> then) {
+        if (condition && !result.isPresent()) {
+            result = then.get();
         }
         return this;
     }
 
     public If elseIf(boolean condition) {
-        return new If(condition, done);
+        return new If(condition, result);
     }
 
-    public void otherwise(Runnable otherwise) {
-        if (!condition && !done) {
-            otherwise.run();
-            done = true;
+    public T otherwise(Runnable otherwise) {
+        if (!result.isPresent()) {
+            result = Optional.of(otherwise.get());
         }
+        return result.orElseThrow(NotExhaustiveIfExpression::new);
     }
 }
